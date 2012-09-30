245dcaccacae701191e9891e64be6cddd6883212
abhayg <abhayg@flipkart.com>
2012-09-30 17:23:03 +0530

    Creating order management module

---
+++ oms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oms::Application.config.secret_token = '4066c7b24918ab2a822088d95c9c50404f9253a1fc00d48cd5b74c98dbb7276365c0b0f8ed6d17e0e157880c2e39fdceab76c574cc3dc59c4241aebd0bc16522'
