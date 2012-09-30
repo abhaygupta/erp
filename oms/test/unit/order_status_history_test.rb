40ab6e9c570499b5a6c7f43bdb9862c4088dbfc5
abhayg <abhayg@flipkart.com>
2012-09-30 21:51:48 +0530

    adding test cases for order validation

--- oms/test/unit/order_status_history_test.rb
+++ oms/test/unit/order_status_history_test.rb
@@ -6,15 +6,16 @@ class OrderStatusHistoryTest < ActiveSupport::TestCase
 
     [:order_id, :status_time, :to_status].each { |column| should validate_presence_of(column) }
     [:from_status, :to_status].each { |column| should ensure_length_of(column).is_at_most(20) }
-    [:created_by].each { |column| should ensure_length_of(column).is_at_most(50) }
+    should ensure_length_of(:created_by).is_at_most(50)
     [:event, :change_reason].each { |column| should ensure_length_of(column).is_at_most(100) }
-    [:comments].each { |column| should ensure_length_of(column).is_at_most(1000) }
+    should ensure_length_of(:comments).is_at_most(1000)
 
     %w(created on_hold approved cancelled completed).each do |value|
       should allow_value(value).for(:from_status)
       should allow_value(value).for(:to_status)
     end
+
+    should belong_to(:order)
   end
 
-  should belong_to(:order)
 end
--- oms/test/unit/order_test.rb
+++ oms/test/unit/order_test.rb
@@ -1,7 +1,27 @@
-require 'test_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../../test/test_helper.rb')
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  context "test order model validations" do
+
+    [:external_id, :order_date, :customer_id, :pickup_address_id, :drop_address_id, :billing_address_id].each { |column| should validate_presence_of(column) }
+    [:external_id, :customer_id, :phone, :email_id, :pickup_address_id, :drop_address_id, :billing_address_id].each { |column| should ensure_length_of(column).is_at_most(100) }
+    [:channel, :status, :currency].each { |column| should ensure_length_of(column).is_at_most(20) }
+    should ensure_length_of(:created_by).is_at_most(50)
+    should ensure_length_of(:comments).is_at_most(1000)
+
+    %w(created on_hold approved cancelled completed).each do |value|
+      should allow_value(value).for(:status)
+    end
+
+    %w(website mobile email).each do |value|
+      should allow_value(value).for(:channel)
+    end
+
+    %w(INR USD).each do |value|
+      should allow_value(value).for(:currency)
+    end
+
+    should have_many(:order_status_histories)
+  end
 end
